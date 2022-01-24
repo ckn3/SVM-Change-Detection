248436da2b25ea8fb70766e2cf7f8eb19081ca4a
jasoncui <33248323+ckn3@users.noreply.github.com>
2022-01-24 17:44:20 +0800

    Add files via upload

--- ReconVersion/SaR2stageMaster.m
+++ ReconVersion/SaR2stageMaster.m
@@ -14,17 +14,41 @@ fig2 = strcat(RegionSelected,'_original_2021-07-23.tif');
 s = size(imread(fig1));
 X_2019 = reshape(double(imread(fig1)),s(1)*s(2),[]);
 X_2021 = reshape(double(imread(fig2)),s(1)*s(2),[]);
-X = [X_2019,X_2021];
 Xu = [rgb2lab(X_2019./255), rgb2lab(X_2021./255)];
 
-clear fig1 fig2 s1 s2 X_2019 X_2021 prompt1
+prompt2 = 'Which dataset? \n 1) RGB Images \n 2) 6-Channel Images \n 3) 10-Channel Images \n ';
+DataSelected = input(prompt2);
+
+if DataSelected == 1
+    fig1 = strcat(RegionSelected,'_original_2019-08-18.tif');
+    fig2 = strcat(RegionSelected,'_original_2021-07-23.tif');
+    s = size(imread(fig1));
+    X_2019 = reshape(double(imread(fig1)),s(1)*s(2),[]);
+    X_2021 = reshape(double(imread(fig2)),s(1)*s(2),[]);
+elseif DataSelected ==2
+    fig1 = load(strcat(RegionSelected,'_original_2019-08-18_ch6.mat'));
+    fig2 = load(strcat(RegionSelected,'_original_2021-07-23_ch6.mat'));
+    s = size(fig1.new);
+    X_2019 = reshape(double(fig1.new),s(1)*s(2),[]);
+    X_2021 = reshape(double(fig2.new),s(1)*s(2),[]);
+elseif DataSelected == 3 
+    fig1 = load(strcat(RegionSelected,'_original_2019-08-18_ch10.mat'));
+    fig2 = load(strcat(RegionSelected,'_original_2021-07-23_ch10.mat'));
+    s = size(fig1.new);
+    X_2019 = reshape(double(fig1.new),s(1)*s(2),[]);
+    X_2021 = reshape(double(fig2.new),s(1)*s(2),[]);
+else
+    disp('Incorrect prompt input. Please enter one of [1:3].')
+end
+
+X = [X_2019,X_2021];
+
+clear fig1 fig2 s1 s2 X_2019 X_2021 prompt1 prompt2
 
 
 % Load and process the labels
 prompt = 'Select the task \n 1) Binary Classification \n 2) 4-class Classification \n';
 choice = input(prompt);
-% For binary classification, take abs value of the difference.
-% For multiclass classification, take the difference.
 if choice == 1
     Y = reshape(double(imread(strcat(RegionSelected,'_original_Binary_change_thr.png'))),s(1)*s(2),1);
     Y = Y/255;
